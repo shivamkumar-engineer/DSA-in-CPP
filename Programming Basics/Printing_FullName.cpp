fbb8d5e8be573fde8a133cc9362ab90d04524bd1
shivamkumar-engineer <shivamkumar.engineer@gmail.com>
2025-05-16 20:23:16 +0530

    3rd commit

---
+++ Programming Basics/Printing_FullName.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char f_name, m_name, l_name, FUll_NAME;
+    f_name = 'Shivam';
+    m_name = '_';
+    l_name = 'Kumar';
+    cout << "My full name is " << f_name << m_name << l_name << endl;
+    return  0;
+}
