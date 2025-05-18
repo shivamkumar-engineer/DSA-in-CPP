c27449cc14e9040954877799eebac63db1724397
shivamkumar-engineer <shivamkumar.engineer@gmail.com>
2025-05-19 03:07:16 +0530

    4th commit

---
+++ Programming Basics/Programming Basics/Input.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout << "Enter the age" << endl;
+    cin >> age;
+    cout << "My age is " << age << " yrs." << endl;
+    return 0;
+}
--- Programming Basics/Programming Basics/Printing_FullName.cpp
+++ Programming Basics/Programming Basics/Printing_FullName.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main(){
     char f_name, m_name, l_name, FUll_NAME;
-    f_name = 'S';
+    f_name = 'Shivam';
     m_name = '_';
-    l_name = 'K';
+    l_name = 'Kumar';
     cout << "My full name is " << f_name << m_name << l_name << endl;
     return  0;
 }
