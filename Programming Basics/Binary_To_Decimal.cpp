748888bad473a77728c84c78bac453a946c1285f
shivamkumar-engineer <shivamkumar.engineer@gmail.com>
2025-05-28 03:22:22 +0530

    8th commit

--- Programming Basics/Binary_To_Decimal.cpp
+++ Programming Basics/Binary_To_Decimal.cpp
@@ -25,35 +25,8 @@ int main(){
     else{
         cout << "Success!!" << endl;
     }
-    
+
     int val = BinaryToDecimal(N);
     cout << val << endl;
     return 0;
 }
-
-
-
-
-// #include<iostream>
-// #include<math.h>
-// using namespace std;
-
-// int BinaryToDecimal(int N){
-//     int decimal = 0;
-//     int i = 0;
-//     while(N){
-//         int bit = N & 1;
-//         decimal += bit*pow(2, i++);
-//         N = N << 1;
-//     }
-//     return decimal;
-// }
-
-// int main(){
-//     int N;
-//     cout << "Enter the Binary No. : " << endl;
-//     cin >> N;
-//     int val = BinaryToDecimal(N);
-//     cout << val << endl;
-//     return 0;
-// }
---
+++ Programming Basics/If_Else-if.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout << "Enter age : " << endl;
+    cin >> age;
+    cout << ((age >= 18?"Vote":"Cannot_Vote")) << endl;
+
+    // if(age >= 18){
+    //     if(age > 18){
+    //         cout << "Kind a Adult" << endl;
+    //     }
+    //     else{
+    //         cout << "Not yet Adult" << endl;
+    //     }
+    // }
+    // else{
+    //     cout << "Still Minor" << endl;
+    // }
+    return 0;
+}
+
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int age;
+//     cout << "Enter age : " << endl;
+//     // cin >> age;
+//     if(cin >> age);
+//     {
+//         // cout << "Vote" << endl;
+//         if(age >= 18){
+//             cout << "Adult" << endl;
+//         }
+//         else{
+//             cout << "Not yet Adult" << endl;
+//         }
+//     }
+//     // else{
+//     //     cout << "Still Minor" << endl;
+//     // }
+//     return 0;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int age;
+//     cout << "Enter age : " << endl;
+//     cin >> age;
+//     if(age >= 18){
+//         if(age > 18){
+//             cout << "Kind a Adult" << endl;
+//         }
+//         else{
+//             cout << "Not yet Adult" << endl;
+//         }
+//     }
+//     else{
+//         cout << "Still Minor" << endl;
+//     }
+//     return 0;
+// }
